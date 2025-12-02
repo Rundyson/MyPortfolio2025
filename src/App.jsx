b7555e01f92ac722b7f5b04ac0bca375b05cb32c
Rundyson <0321-3589@lspu.edu.ph>
2025-12-02 16:42:58 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import { BrowserRouter as  Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './components/frontend/HomePage/HomePage'
 import ProjectsSection from './components/frontend/HomePage/ProjectSection'
 
-
 const App = () => {
   return (
-  <Router>
-    <Routes>
-      <Route index element={<HomePage/>}/>
-      <Route path="/projects" element={<ProjectsSection/>}/>
-    </Routes>
-  </Router>
+    <Router>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="projects" element={<ProjectsSection />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/frontend/HomePage/AboutMe.jsx
+++ src/components/frontend/HomePage/AboutMe.jsx
@@ -9,8 +9,7 @@ const AboutMe = ({ id }) => {
       className=" py-1 bg-lightGray dark:bg-matteBlack transition-colors duration-500 text-black dark:text-white"
     >
       <div className="seperate md:flex md:flex-row sm:flex sm:flex-col-reverse gap-5 container ">
-        <div className="left md:w-1/2 sm:w-full md:border-r-2 sm:border-t-2 md:pr-7 sm:px-6 flex flex-col gap-10">
-         
+        <div className="left md:w-1/2 sm:w-full md:border-r-2 sm:border-r-0 sm:border-t-2 md:border-t-0 md:pr-7 sm:px-6 flex flex-col gap-10">
           <div
             data-aos="fade-right"
             className="card md:mt-0 sm:mt-5 center-all flex flex-col bg-white dark:bg-gray-300/40 rounded-lg shadow-sm p-4"
@@ -25,7 +24,6 @@ const AboutMe = ({ id }) => {
             />
           </div>
 
-        
           <div
             data-aos="fade-up"
             data-aos-duration="1500"
@@ -38,34 +36,32 @@ const AboutMe = ({ id }) => {
           </div>
         </div>
 
-<div className="right md:w-1/2 sm:w-full center-all flex flex-col gap-5">
+        <div className="right md:w-1/2 sm:w-full center-all flex flex-col gap-5">
+          <div
+            data-aos="fade-up"
+            className="bg-white dark:bg-gray-300/40 rounded-lg shadow-sm p-6 flex flex-col items-center gap-5 border border-gray-300 dark:border-gray-500 w-full"
+          >
+            <h2 className="text-2xl font-semibold">About me</h2>
 
-  <div
-    data-aos="fade-up"
-    className="bg-white dark:bg-gray-300/40 rounded-lg shadow-sm p-6 flex flex-col items-center gap-5 border border-gray-300 dark:border-gray-500 w-full"
-  >
-    <h2 className="text-2xl font-semibold">About me</h2>
+            <img
+              className="md:max-w-[300px] sm:max-w-[200px] rounded-full border border-gray-200 dark:border-gray-400 shadow-sm"
+              src="/images/graduate.jpg"
+              alt="Graduate"
+            />
 
-    <img
-      className="md:max-w-[300px] sm:max-w-[200px] rounded-full border border-gray-200 dark:border-gray-400 shadow-sm"
-      src="/images/graduate.jpg"
-      alt="Graduate"
-    />
+            <p className="italic text-center text-sm md:text-base sm:text-justify sm:text-xs">
+              I’m Randyson D. Baful, a web developer with practical experience
+              from an internship at Frontline Business Inc. and work at
+              McDonald’s. These roles strengthened my teamwork, communication,
+              and technical skills. I’m passionate about continuous learning,
+              adapting quickly, and delivering effective solutions in dynamic
+              environments.
+            </p>
+          </div>
 
-    <p className="italic text-center text-sm md:text-base sm:text-justify sm:text-xs">
-      I’m Randyson D. Baful, a web developer with practical experience
-      from an internship at Frontline Business Inc. and work at
-      McDonald’s. These roles strengthened my teamwork, communication, and
-      technical skills. I’m passionate about continuous learning, adapting
-      quickly, and delivering effective solutions in dynamic environments.
-    </p>
-  </div>
-
-
-  <h2 className="mt-4 mb-0 text-2xl font-semibold">Skills</h2>
-  <SkillsCards />
-</div>
-
+          <h2 className="mt-4 mb-0 text-2xl font-semibold">Skills</h2>
+          <SkillsCards />
+        </div>
       </div>
     </section>
   )
